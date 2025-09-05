7dc5e0d571aee6bb409de0a4bbdb82a3344373d3
Manuel Couto <mcouto1@mail.austral.edu.ar>
2025-09-05 12:38:14 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -9,8 +9,8 @@ def check_vowels():
     tiene_o = "o" in nombre
     tiene_u = "u" in nombre
 
-    print(f"Contiene a:", tiene_a)
-    print(f"Contiene e:", tiene_e)
-    print(f"Contiene i:", tiene_i)
-    print(f"Contiene o:", tiene_o)
-    print(f"Contiene u:", tiene_u)
+    print("Contiene a:", tiene_a)
+    print("Contiene e:", tiene_e)
+    print("Contiene i:", tiene_i)
+    print("Contiene o:", tiene_o)
+    print("Contiene u:", tiene_u)
